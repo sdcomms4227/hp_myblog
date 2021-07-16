3ae35e8f9da8d9175ac3c92d977453101d1db13c
sdcomms4227 <sdcomms4227@gmail.com>
2021-07-16 15:46:52 +0900

    10 JPA 리파지터리, 데이터 저장하기 완료

--- src/main/java/com/example/hp_myblog/api/ArticleApiController.java
+++ src/main/java/com/example/hp_myblog/api/ArticleApiController.java
@@ -1,7 +1,10 @@
 package com.example.hp_myblog.api;
 
 import com.example.hp_myblog.dto.ArticleForm;
+import com.example.hp_myblog.entity.Article;
+import com.example.hp_myblog.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArticleApiController {
 
+    @Autowired // 리파티터리 객체를 알아서 가져옴! 자바는 new ArticleRepository() 해야 했음!
+    private ArticleRepository articleRepository;
+
     @PostMapping("/api/articles") // Post 요청이 "/api/articles" url로 온다면, 메소드 수행!
     public Long create(@RequestBody ArticleForm form){ // JSON 데이터를 받아옴!
         log.info(form.toString()); // 받아온 데이터 확인!
-        return 0L;
+
+        Article article = form.toEntity();
+
+        // 리파티터리에게(db-관리-객체) 전달
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+
+        //저장 엔티티의 id(PK)값 반환!
+        return saved.getId();
     }
 }
--- src/main/java/com/example/hp_myblog/dto/ArticleForm.java
+++ src/main/java/com/example/hp_myblog/dto/ArticleForm.java
@@ -1,46 +1,20 @@
 package com.example.hp_myblog.dto;
 
+import com.example.hp_myblog.entity.Article;
+import lombok.Data;
+
+@Data // 생성자(지폴트, All), 게터, 세터, toString 등 다 만들어 줌!
 public class ArticleForm {
     private String author;
     private String title;
     private String content;
 
-    public ArticleForm(String author, String title, String content) {
-        this.author = author;
-        this.title = title;
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "author='" + author + '\'' +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
+    public Article toEntity() {
+        return Article.builder()
+                .id(null)
+                .author(author)
+                .title(title)
+                .content(content)
+                .build();
     }
 }
