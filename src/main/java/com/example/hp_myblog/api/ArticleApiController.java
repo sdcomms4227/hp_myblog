bc3d73be708eb1fbedb5c5ff465459994ed03263
sdcomms4227 <sdcomms4227@gmail.com>
2021-07-19 18:43:08 +0900

    16 서비스 레이어, 데이터 수정하기 완료

--- src/main/java/com/example/hp_myblog/api/ArticleApiController.java
+++ src/main/java/com/example/hp_myblog/api/ArticleApiController.java
@@ -3,6 +3,8 @@ package com.example.hp_myblog.api;
 import com.example.hp_myblog.dto.ArticleForm;
 import com.example.hp_myblog.entity.Article;
 import com.example.hp_myblog.repository.ArticleRepository;
+import com.example.hp_myblog.service.ArticleService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,15 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 public class ArticleApiController {
 
     @Autowired // 리파티터리 객체를 알아서 가져옴! 자바는 new ArticleRepository() 해야 했음!
     private ArticleRepository articleRepository;
+
+    // 서비스 레이어 연결! 서비스 레이어란?
+    private final ArticleService articleService;
 
     @PostMapping("/api/articles") // Post 요청이 "/api/articles" url로 온다면, 메소드 수행!
     public Long create(@RequestBody ArticleForm form){ // JSON 데이터를 받아옴!
@@ -51,7 +57,7 @@ public class ArticleApiController {
 
     @PutMapping("/api/articles/{id}") // HTTP 메소드 PUT으로 "/api/articles/{id}" 요청이 들어오면 수행!
     public Long update(@PathVariable Long id, @RequestBody ArticleForm form){
-        log.info("form: " + form.toString()); // 받아온 데이터 확인!
-        return 0L;
+        Article saved = articleService.update(id, form); // 서비스 객체가 update를 수행
+        return saved.getId();
     }
 }
