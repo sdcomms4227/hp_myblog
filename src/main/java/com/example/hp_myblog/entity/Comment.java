c6f8d053486ab883bf2768e1b922616e1f5bb9e7
sdcomms4227 <sdcomms4227@gmail.com>
2021-07-21 11:12:01 +0900

    24 연관 관계 설정하기 완료

--- src/main/java/com/example/hp_myblog/entity/Comment.java
+++ src/main/java/com/example/hp_myblog/entity/Comment.java
@@ -20,4 +20,14 @@ public class Comment extends BaseTime {
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
+    // 다대일 관계 설정
+    @ManyToOne // 여러 Comment가 하나의 Article 에 포함 됨!
+    @JoinColumn(name = "article_id") // 포함 대상 정보는 article_id에 기록!
+    private  Article article;
+
+    // 해당 댓글이 어느 게시글에 작성된 것인지를 기록!
+    public void stickTo(Article article) {
+        this.article = article;
+    }
 }
